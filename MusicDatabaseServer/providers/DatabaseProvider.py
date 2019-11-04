965e0cce7d2c482dd814ca9651b67d2dfbab4600
thrasher <thrasher@thrasher-All-Series>
2019-11-04 20:06:41 +0100

    Changed interface definition to reflect the fav db contents

--- MusicDatabaseServer/providers/DatabaseProvider.py
+++ MusicDatabaseServer/providers/DatabaseProvider.py
@@ -2,11 +2,8 @@
 song = {
         "_id":"123",
         "title": "Northern Chaos gods",
-        "band": "Immortal",
-        "album": "Northern Chaos Gods",
-        "total_lenght": "4:34",
-        "abs_path": "/home/thrasher/Downloads/2018\ -\ Northern\ Chaos\ Gods/01\ -\ Northern\ Chaos\ Gods\.mp3",
-        "score":"7.5"
+        "score":"7.5",
+        "disc_id":"13456"
         }
 
 
