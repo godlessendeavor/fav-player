12188dc4bd9797ae53785cf395ffd90500de8dbf
thrasher <thrasher@thrasher-All-Series>
2019-11-12 16:16:52 +0100

    Config for Music Database Added peewee framework and fetch random fav songs done

---
+++ MusicDatabaseServer/config/config.py
@@ -0,0 +1,26 @@
+'''
+Created on Nov 12, 2019
+
+@author: thrasher
+'''
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# config.py: configuration data of this app
+#   - the config is first read from a json file
+#   - env variables may override (in docker setup etc)
+
+import json
+
+
+# first load config from a json file,
+#appconf = json.load(open(os.environ["MUSICDB_CONFIG_PATH"]))
+appconf = json.load(open('/home/thrasher/musicdb.conf'))
+
+# database config
+DATABASE_HOST = appconf['music_db']['DATABASE_HOST']
+DATABASE_PORT = appconf['music_db']['DATABASE_PORT']
+DATABASE_NAME = appconf['music_db']['DATABASE_NAME']
+DATABASE_USER = appconf['music_db']['DATABASE_USER']
+DATABASE_PASSWORD = appconf['music_db']['DATABASE_PASSWORD']
+
--- MusicDatabaseServer/providers/DatabaseProvider.py
+++ MusicDatabaseServer/providers/DatabaseProvider.py
@@ -1,4 +1,16 @@
-#import requests
+from peewee import *
+from playhouse.shortcuts import model_to_dict, dict_to_model
+from config import config
+import json
+
+
+import logging
+from _cffi_backend import typeof
+logging.basicConfig(
+    format='[%(asctime)-15s] [%(name)s] %(levelname)s]: %(message)s',
+    level=logging.DEBUG
+)
+
 song = {
         "_id":"123",
         "title": "Northern Chaos gods",
@@ -12,10 +24,59 @@ album = {
                  }
 
 
+
+database = MySQLDatabase('music', 
+                         **{'charset': 'utf8', 
+                            'sql_mode': 'PIPES_AS_CONCAT,NO_AUTO_CREATE_USER', 
+                            'use_unicode': True, 
+                            'host': config.DATABASE_HOST, 
+                            'port': config.DATABASE_PORT, 
+                            'user': config.DATABASE_USER, 
+                            'password': config.DATABASE_PASSWORD})
+
+
+class BaseModel(Model):
+    class Meta:
+        database = database
+        
+class Music(BaseModel):
+    id = AutoField(column_name='Id')
+    copy = CharField(constraints=[SQL("DEFAULT ' '")])
+    group_name = CharField(column_name='groupName', constraints=[SQL("DEFAULT ' '")])
+    loc = CharField(constraints=[SQL("DEFAULT ' '")])
+    mark = CharField(constraints=[SQL("DEFAULT ' '")])
+    review = TextField(null=True)
+    style = CharField(constraints=[SQL("DEFAULT ' '")])
+    title = CharField(constraints=[SQL("DEFAULT ' '")])
+    type = CharField(constraints=[SQL("DEFAULT ' '")])
+    year = IntegerField(constraints=[SQL("DEFAULT 0")], null=True)
+
+    class Meta:
+        table_name = 'music'
+
+class Favorites(BaseModel):
+    id = AutoField(column_name='Id')
+    disc_id = ForeignKeyField(Music, backref='favorites')
+    score = FloatField(constraints=[SQL("DEFAULT 0")])
+    track_no = IntegerField(constraints=[SQL("DEFAULT 0")])
+    track_title = CharField()
+    type = TextField(null=True)
+
+    class Meta:
+        table_name = 'favorites'
+
+
+
+database.connect()
+
+
 class DatabaseProvider(object):
     
     def get_songs(self, quantity, score) -> str:
-        return song,200
+        #get result from database as Peewee model
+        result = Favorites.select().where(Favorites.score > score).order_by(fn.Rand()).limit(int(quantity))
+        list_result = [row for row  in result.dicts()]
+        return json.dumps(list_result),200
     
     def create_song(self, song) ->str:
         print(song)
