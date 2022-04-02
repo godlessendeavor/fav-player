9c900f12a9b956c5aea7591c9f88721c7bee2bbb
Jose Bardanca <jlvbardanca@gmail.com>
2022-04-02 16:27:54 +0000

    Fixes on rclone script

--- mysql-rclone-backup.sh
+++ mysql-rclone-backup.sh
@@ -2,38 +2,40 @@
 ##this script will backup mysql and upload it to google drive 
 ##mysql container id
 CONTAINER=$1
+## directory path
+DIRPATH=$2
 ##directory name
-DIRNAME=$2;
+DIRNAME=$3;
 ##database name
-DATABASEe=$3;
+DATABASE=$4;
 ##database username
-DBUSER=$4;
+DBUSER=$5;
 ##database password
-DBPASS=$5;
+DBPASS=$6;
 ## rclone remote name
-RCLONE_REMOTE_NAME=$6;
+RCLONE_REMOTE_NAME=$7;
 ##google drive folder name
-GDRIVE_FOLDER_NAME=$7;
+GDRIVE_FOLDER_NAME=$8;
 ##condition to check folder exist or not 
-if [ ! -d "$DIRNAME" ]
+if [ ! -d "$DIRPATH$DIRNAME" ]
 then
     ##create directory
-    mkdir ./$DIRNAME
+    mkdir $DIRPATH$DIRNAME
     ##dump mysql database on server
-    docker exec $CONTAINER /usr/bin/mysqldump -u $DBUSER -p$DBPASS $DATABASEe | gzip>"./$DIRNAME/$DATABASEe($(date +\%Y_\%m_\%d_\%H)).sql.gz"
+    docker exec $CONTAINER /usr/bin/mysqldump -u $DBUSER -p$DBPASS $DATABASEe | gzip>"$DIRPATH$DIRNAME/$DATABASEe($(date +\%Y_\%m_\%d_\%H)).sql.gz"
     ##wait for 10 seconds
     sleep 10
     ##upload it to google drive
-    rclone copy "./$DIRNAME/$DATABASEe($(date +\%Y_\%m_\%d_\%H)).sql.gz" $RCLONE_REMOTE_NAME:$GDRIVE_FOLDER_NAME
+    rclone copy "$DIRPATH$DIRNAME/$DATABASEe($(date +\%Y_\%m_\%d_\%H)).sql.gz" $RCLONE_REMOTE_NAME:$GDRIVE_FOLDER_NAME
 ##if folder already exist
 else
     ##dump mysql database on server
-    mysqldump -u $DBUSER -p$DBPASS $DATABASEe | gzip>"./$DIRNAME/$DATABASEe($(date +\%Y-\%m-\%d-\%H)).sql.gz"
+    docker exec $CONTAINER /usr/bin/mysqldump -u $DBUSER -p$DBPASS $DATABASEe | gzip>"$DIRPATH$DIRNAME/$DATABASEe($(date +\%Y-\%m-\%d-\%H)).sql.gz"
      ##wait for 10 seconds
     sleep 10
     ##upload it to google drive
-    rclone copy "./$DIRNAME/$DATABASEe($(date +\%Y-\%m-\%d-\%H)).sql.gz" $RCLONE_REMOTE_NAME:$GDRIVE_FOLDER_NAME
+    rclone copy "$DIRPATH$DIRNAME/$DATABASEe($(date +\%Y-\%m-\%d-\%H)).sql.gz" $RCLONE_REMOTE_NAME:$GDRIVE_FOLDER_NAME
     ##delete 10 days older file on server to save disk space
-    find ./$DIRNAME -mtime +10 -type f -delete
+    find $DIRPATH$DIRNAME -mtime +10 -type f -delete
 fi
 exit 0;
